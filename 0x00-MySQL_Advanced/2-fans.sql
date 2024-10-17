91f2aebbfb8885b508b0410c85a6e2177a9e3ae0
Great <jgreat7702gmail.com>
2024-10-17 12:33:52 +0100

    task2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,10 @@
--- Script to rank country origins of bands, ordered by the number of non-unique fans
+# Import the table dump
+mysql -uroot -p holberton < metal_bands.sql
 
+# SQL script to rank country origins of bands, ordered by the number of non-unique fans
+echo "
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
+" | mysql -uroot -p holberton
